ff8271bb2c42f4232a07a50fa4ed3e06db4dfc54
daniherr <daniherr@bancolombia.com.co>
2021-03-17 11:30:40 -0500

    subiendo los elementos finales

--- lib/config/use_case_config.dart
+++ lib/config/use_case_config.dart
@@ -1,7 +1,5 @@
-
-import 'package:flutter_clean_arch/domain/use_cases/album/get_album_usecase.dart';
+import 'package:flutter_clean_arch/domain/use_cases/album/get_album_use_case.dart';
 import 'package:flutter_clean_arch/infraestructure/driven_adapter/album_api/album_api.dart';
-
 class UseCaseConfig {
   GetAlbumUseCase getAlbumUseCase;
   AlbumApi _albumApi;
--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_arch/config/usecase_config.dart';
+import 'package:flutter_clean_arch/config/use_case_config.dart';
 import 'package:flutter_clean_arch/ui/common/molecules/our_footer.dart';
 import 'package:flutter_clean_arch/ui/common/molecules/our_header.dart';
 import 'package:flutter_clean_arch/ui/helpers/async_snapshot_album_helper.dart';
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     UseCaseConfig _albumConfig = UseCaseConfig();
