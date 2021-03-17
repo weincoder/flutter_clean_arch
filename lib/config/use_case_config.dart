092265648b0cd4579d36e65e3002b14cb8245ffe
Danielherresan <danielherresan@gmail.com>
2021-03-17 17:05:01 -0500

    subiendo los elementos

--- lib/config/use_case_config.dart
+++ lib/config/use_case_config.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_clean_arch/domain/use_cases/album/get_album_use_case.dart';
 import 'package:flutter_clean_arch/infraestructure/driven_adapter/album_api/album_api.dart';
+import 'package:flutter_clean_arch/infraestructure/driven_adapter/album_api/album_api_delay.dart';
 class UseCaseConfig {
   GetAlbumUseCase getAlbumUseCase;
-  AlbumApi _albumApi;
+  AlbumApiDelay _albumApi;
   UseCaseConfig (){
-    _albumApi = AlbumApi();
+    _albumApi = AlbumApiDelay();
     getAlbumUseCase = GetAlbumUseCase(_albumApi) ;
   }
 }
